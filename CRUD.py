fef56a050ebf5bdb6585fc08a7b7f6bb261a8e1c
karintiara <karin.rahmadina.000@gmail.com>
2024-05-22 14:44:45 +0700

    tambah fitur read -alif

--- CRUD.py
+++ CRUD.py
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 
 def delete_pengepul(admin):
-    read_pengepul(cur)
+    read_admin(cur)
     id_pengepul = input('Masukkan id pengepul yang ingin dihapus: ') 
     delete_pengepul = f"DELETE FROM admin WHERE id_pengepul = {id_pengepul}"
     cur.execute(delete_pengepul)
 
     conn.commit()
     print(f"total baris yang dihapus: {cur.rowcount}")
-=======
+
 from Connection import *
 
-# parameter nda perlu menyesuaikan tablenya parameter dikosongn aja
-def update_admin(admin):
+def update_admin():
     id_pengepul =       input("Masukkan Id Pengepul       : ")
     nama =              input("Masukkan Nama Pengepul     : ") 
     password_admin =    input("Masukkan Password Pengepul : ") 
@@ -31,7 +30,7 @@ def update_admin(admin):
     cur.close()
     conn.close()
 
-def update_pegawai_penjemputan (pegawai_penjemputan):
+def update_pegawai_penjemputan ():
     id_pegawai =        input("Masukkan Id Pegawai   : ")
     nama_pegawai =      input("Masukkan Nama Pegawai : ") 
     no_telepon =        input("Masukkan No Telepon   : ") 
@@ -48,8 +47,7 @@ def update_pegawai_penjemputan (pegawai_penjemputan):
     cur.close()
     conn.close()
 
-def update_cash (cash):
-    # id_pembayaran = input("Masukkan Id Pegawai: ")
+def update_cash ():
     id_cash =           input("Masukkan Id Cash           : ") 
     nomor_telepon =     input("Masukkan No Telepon        : ") 
     jumlah_pembayaran = input("Masukkan Jumlah Pembayaran : ")
@@ -65,8 +63,7 @@ def update_cash (cash):
     conn.close()
 
 
-def update_debit (debit):
-    # id_pembayaran = input(f"Masukkan Id Pegawai: ")
+def update_debit ():
     id_debit =       input("Masukkan Id Debit       : ") 
     nama_pemilik =   input("Masukkan Nama           : ") 
     nama_bank =      input("Masukkan Nama Bank      : ")
@@ -82,7 +79,7 @@ def update_debit (debit):
     cur.close()
     conn.close()
 
-def update_gudang (gudang):
+def update_gudang ():
     kode_gudang = input("Masukkan Kode Gudang   : ")
     kapasitas =   input("Masukkan Kapasitas     : ") 
     deskripsi =   input("Masukkan Deskripsi     : ") 
@@ -97,7 +94,7 @@ def update_gudang (gudang):
     cur.close()
     conn.close()
 
-def update_jadwal_penjemputan (jadwal_penjemputan):
+def update_jadwal_penjemputan ():
     id_penjemputan =                           input("Masukkan Id Penjemputan           : ")
     waktu_penjemputan =                        input("Masukkan Waktu Penjemputan        : ") 
     status_penjemputan_id_status_penjemputan = input("Masukkan Id Status Penjemputan    : ") 
@@ -112,7 +109,7 @@ def update_jadwal_penjemputan (jadwal_penjemputan):
     cur.close()
     conn.close()
 
-def update_pembayaran (pembayaran):
+def update_pembayaran ():
     id_pembayaran =     input("Masukkan Id Pembayaran       : ")
     metode_pembayaran = input("Masukkan Metode Pembayaran   : ") 
     deskripsi =         input("Masukkan Deskripsi           : ") 
@@ -127,7 +124,7 @@ def update_pembayaran (pembayaran):
     cur.close()
     conn.close()
 
-def update_service (service):
+def update_service ():
     id_layanan =    input("Masukkan Id Layanan      : ")
     nama_layanan =  input("Masukkan Nama Layanan    : ") 
     deskripsi =     input("Masukkan Deskripsi       : ") 
@@ -142,7 +139,7 @@ def update_service (service):
     cur.close()
     conn.close()
 
-def update_status_penjemputan (status_penjemputan):
+def update_status_penjemputan ():
     id_status_penjemputan = input("Masukkan Id Status   : ")
     nama_status =           input("Masukkan Nama Status : ") 
 
@@ -156,7 +153,7 @@ def update_status_penjemputan (status_penjemputan):
     cur.close()
     conn.close()
 
-def update_status_transaksi (transaksi):
+def update_status_transaksi ():
     id_transaksi =                      input("Masukkan Id Transaksi    : ")
     deskripsi =                         input("Masukkan Deskripsi       : ") 
     user_id_petani =                    input("Masukkan Id Petani       : ") 
@@ -178,7 +175,7 @@ def update_status_transaksi (transaksi):
     cur.close()
     conn.close()
 
-def update_User (User):
+def update_User ():
     id_petani =     input("Masukkan Id Petani       : ")
     nama =          input("Masukkan Nama            : ") 
     password_user = input("Masukkan Password        : ") 
@@ -192,15 +189,14 @@ def update_User (User):
 
     conn.commit()
     print("transaksi berhasil diupdate")
->>>>>>> 0a25802316c15be663339d845f48aab8d3ba0e1f
+#>>>>>>> 0a25802316c15be663339d845f48aab8d3ba0e1f
 
     cur.close()
     conn.close()
 
 
-
-def delete_pegawai(pegawai_penjemputan):
-    read_pegawai(cur)
+def delete_pegawai():
+    read_pegawai_penjemputan(cur)
     id_pegawai = input('Masukan id pegawai yang ingin dihapus')
     delete_pegawai = f"DELETE FROM pegawai_penjemputan WHERE id_pegawai = {id_pegawai}"
     cur.execute(delete_pegawai)
@@ -213,7 +209,7 @@ def delete_pegawai(pegawai_penjemputan):
 
 
 def delete_layanan(service):
-    read_layanan(cur)
+    read_service(cur)
     id_layanan = input('Masukan id layanan yang ingin dihapus: ')
     delete_layanan = f"DELETE FROM service WHERE id_layanan = {id_layanan}"
     cur.execute(delete_layanan)
@@ -238,7 +234,7 @@ def delete_transaksi(transaksi):
 
 
 def delete_petani(User):
-    read_petani(cur)
+    read_User(cur)
     id_petani = input('Masukan id Petani yang ingin dihapus: ')
     delete_petani = f"DELETE FROM User WHERE id_petani = {id_petani}"
     cur.execute(delete_petani)
@@ -250,7 +246,7 @@ def delete_petani(User):
     conn.close()
 
 def delete_kualitas(kualitas_kopi):
-    read_kualitas(cur)
+    read_kualitas_kopi(cur)
     id_kualitas = input('Masukan id Kualitas Kopi yang ingin dihapus: ')
     delete_kualitas = f"DELETE FROM kualitas_kopi WHERE id_kualitas = {id_kualitas}"
     cur.execute(delete_kualitas)
@@ -273,7 +269,7 @@ def delete_status_penjemputan(status_penjemputan):
     conn.close()
 
 def delete_jadwal_penjemputan(jadwal_penjemputan):
-    read_penjemputan(cur)
+    read_jadwal_penjemputan(cur)
     id_penjemputan = input('Masukan id Jadwal Penjeputan yang ingin dihapus: ')
     delete_jadwal_penjemputan = f"DELETE FROM jadwal_penjemputan WHERE id_penjemputan = {id_penjemputan}"
     cur.execute(delete_jadwal_penjemputan)
@@ -310,7 +306,7 @@ def delete_pembayaran(pembayaran):
     conn.close()
 
 def delete_pembayaran_cash(cash):
-    read_pembayaran_cash(cur)
+    read_cash(cur)
     id_cash = input('Masukan id Pembayaran cash yang ingin dihapus: ')
     delete_pembayaran_cash = f"DELETE FROM cash WHERE id_cash = {id_cash}"
     cur.execute(delete_pembayaran_cash)
@@ -321,7 +317,7 @@ def delete_pembayaran_cash(cash):
     conn.close()
 
 def delete_pembayaran_debit(debit):
-    read_pembayaran_debit(cur)
+    read_debit(cur)
     id_debit = input('Masukan id Pembayaran Debit yang ingin dihapus: ')
     delete_pembayaran_debit = f"DELETE FROM debit WHERE id_debit = {id_debit}"
     cur.execute(delete_pembayaran_debit)
@@ -341,6 +337,8 @@ def read_kualitas_kopi():
     cur.close()
     conn.close()
 
+total_input = int(input(f"Ingin menambahkan berapa data?: "))
+
 for i in range(total_input):
     id_kualitas = input(f"Masukkan id kualitas: ")
     nama_kualitas = int(input(f"Masukkan nama kualitas: "))
@@ -349,11 +347,11 @@ for i in range(total_input):
    
     cur.execute(query_read_kualitas_kopi) 
 
-conn.commit()
+    conn.commit()
 
-read_kualitas_kopi(cur)
-cur.close()
-conn.close()
+    read_kualitas_kopi(cur)
+    cur.close()
+    conn.close()
 
 
 def read_pegawai_penjemputan(cur):    
@@ -375,11 +373,11 @@ for i in range(total_input):
    
     cur.execute(query_read_pegawai_penjemputan) 
 
-conn.commit()
+    conn.commit()
 
-read_pegawai_penjemputan(cur)
-cur.close()
-conn.close()
+    read_pegawai_penjemputan(cur)
+    cur.close()
+    conn.close()
 
 
 def read_pembayaran(cur):    
@@ -399,11 +397,11 @@ for i in range(total_input):
    
     cur.execute(query_read_pembayaran) 
 
-conn.commit()
+    conn.commit()
 
-read_pembayaran(cur)
-cur.close()
-conn.close()
+    read_pembayaran(cur)
+    cur.close()
+    conn.close()
 
 
 def read_service(cur):    
@@ -423,11 +421,11 @@ for i in range(total_input):
    
     cur.execute(query_read_service) 
 
-conn.commit()
+    conn.commit()
 
-read_kualitas_kopi(cur)
-cur.close()
-conn.close()
+    read_kualitas_kopi(cur)
+    cur.close()
+    conn.close()
 
 
 def read_status_penjemputan(cur):    
@@ -446,11 +444,11 @@ for i in range(total_input):
    
     cur.execute(query_read_status_penjemputan) 
 
-conn.commit()
+    conn.commit()
 
-read_status_penjemputan(cur)
-cur.close()
-conn.close()
+    read_status_penjemputan(cur)
+    cur.close()
+    conn.close()
 
 
 def read_transaksi(cur):    
@@ -477,11 +475,164 @@ for i in range(total_input):
    
     cur.execute(query_read_transaksi) 
 
-conn.commit()
+    conn.commit()
 
-read_transaksi(cur)
-cur.close()
-conn.close()
+    read_transaksi(cur)
+    cur.close()
+    conn.close()
 
+# SELECT USER
+def read_User(cur):    
+    query = "SELECT * FROM User"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    conn.close()
 
+total_input = int(input(f"Ingin menambahkan berapa data?: "))
+
+for i in range(total_input):
+    id_petani = int(input(f"Masukkan Id petani: "))
+    nama = input(f"Masukkan nama petani: ")
+    password_user = input(f"Masukkan password: ")
+    email = input(f"Masukkan email")
+    nomor_telepon = int(input("nomor telepon"))
+    deskripsi = input(f"Masukkan deskripsi")
+    query = f"INSERT INTO User(id_petani, nama, password_user, email, nomor_telepon, deskripsi) VALUES('{id_petani}', {nama}, {password_user}, {email}, {nomor_telepon}, {deskripsi})"
+    # query = f"INSERT INTO User(id_petani, nama, password_user, email, nomor_telepon, deskripsi) VALUES(%s, %s, %s)"
+    cur.execute(query, (id_petani, nama, password_user, email, nomor_telepon, deskripsi))
+
+    conn.commit()
+
+    read_User(cur)
+    cur.close()
+    conn.close()
+
+# SELECT ADMIN
+def read_admin(cur):
+    query = "SELECT * FROM admin"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    conn.close()
+
+total_input = int(input(f"Ingin menambahkan berapa data?: "))
+
+for i in range(total_input):
+    id_pengepul = int(input(f"Masukkan Id pengepul: "))
+    nama = input(f"Masukkan nama pengepul: ")
+    password_admin = input(f"Masukkan password admin: ")
+    email = input(f"Masukkan email: ")
+    nomor_telepon = int(input(f"Masukkan nomor telepon: "))
+    deskripsi1 = input(f"Masukkan deskripsi: ")
+    # query = f"INSERT INTO admin(id_pengepul, nama, password_admin, email, nomor_telepon, deskripsi1) VALUES(%s, %s, %s)"
+    cur.execute(query, (id_pengepul, nama, password_admin, email, nomor_telepon, deskripsi1))
+
+    conn.commit()
+
+    read_admin(cur)
+    cur.close()
+    conn.close()
+
+# SELECT CASH
+def read_cash(cur):
+    query = "SELECT * FROM cash"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    conn.close()
+
+for i in range(total_input):
+    id_pembayaran = int(input(f"Masukkan id pembayaran: "))
+    id_cash = int(input(f"Masukkan id cash: "))
+    nomor_telepon = int(input(f"Masukkan nomor telepon: "))
+    jumlah_pembayaran = int(input(f"Masukkan jumlsh pembayaran: "))   
+    # query = f"INSERT INTO User(id_pembayaran, id_cash, nomor_telepon, jumlah_pembayaran) VALUES(%s, %s, %s)"
+    cur.execute(query, (id_pembayaran, id_cash, nomor_telepon, jumlah_pembayaran))
+
+    conn.commit()
+
+    read_cash(cur)
+    cur.close()
+    conn.close()
+
+# SELECT DEBIT
+def read_debit(cur):
+    query = "SELECT * FROM debit"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    conn.close()
+
+for i in range(total_input):
+    id_pembayaran = int(input(f"Masukkan Id pembayaran: "))
+    id_debit = int(input(f"Masukkan Id debit: "))
+    nama_pemilik = input(f"Masukkan nama pemilik: ")
+    nama_bank = input(f"Masukkan nama bank: ")
+    nomor_rekening = int(input(f"Masukkan Id pengepul: "))
+    
+# query = f"INSERT INTO debit(id_pembayaran, id_debit, nama_pemilik, nama_bank, nomor_rekening) VALUES(%s, %s, %s)"
+    cur.execute(query, (id_pembayaran, id_debit, nama_pemilik, nama_bank, nomor_rekening))
+
+    conn.commit()
+
+    read_debit(cur)
+    cur.close()
+    conn.close()
+
+# SELECT GUDANG
+def read_gudang(cur):
+    query = "SELECT * FROM gudang"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    conn.close()
+
+for i in range(total_input):
+    kode_gudang = int(input(f"Masukkan kode gudang: "))
+    kapasitas = int(input(f"Masukkan kapasitas: "))
+    deskripsi = input(f"Masukkan deskripsi: ")
+
+    # query = f"INSERT INTO gudang(kode_gudang, kapasitas, deskripsi) VALUES(%s, %s, %s)"
+    cur.execute(query, (kode_gudang, kapasitas, deskripsi))
+
+    conn.commit()
+
+    read_gudang(cur)
+    cur.close()
+    conn.close()
+
+# SELECT JADWAL PENJEMPUTAN
+def read_jadwal_penjemputan(cur):
+    query = "SELECT * FROM jadwal_penjemputan"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    conn.close()
+
+for i in range(total_input):
+    id_penjemputan = int(input(f"Masukkan Id penjeputan: "))
+    waktu_penjemputan = input(f"Masukkan waktu penjemputan: ")
+    status_penjemputan = int(input(f"Masukkan status penjemputan: "))
+
+    # query = f"INSERT INTO penjemputan(id_penjemputan, waktu_penjemputan, status_penjemputan) VALUES(%s, %s, %s)"
+    cur.execute(query, (id_penjemputan, waktu_penjemputan, status_penjemputan))
+
+    conn.commit()
+
+    read_jadwal_penjemputan(cur)
+    cur.close()
+    conn.close()
 
